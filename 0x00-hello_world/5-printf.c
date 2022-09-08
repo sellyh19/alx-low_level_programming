a383ada796adfec4c036d6b5b8d75bf5f4cfd702
Selma Shikone <sellyh19@gmail.com>
2022-09-08 14:29:28 -0700

    print code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/** code to prints exactly with proper grammar
- * Done by Selma
+/**
  * main- main block
  * return:0
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
