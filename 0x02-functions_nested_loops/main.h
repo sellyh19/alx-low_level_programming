0214013effd96e5f2bfec4ceedc0da7e6c5cf41b
Selma Shikone <sellyh19@gmail.com>
2022-09-13 15:14:39 -0700

    alphabet in lower case

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef main.h
-#define main.h
-char _putchar(char c);
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,4 +14,6 @@ void times_table(void);
 int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
+
 #endif
