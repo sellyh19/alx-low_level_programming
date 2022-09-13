4de79c3ead2f93beb04487050cea5ebbc8832c3e
Selma Shikone <sellyh19@gmail.com>
2022-09-13 16:51:20 -0700

    print every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - func to print every minute of the day Jack Bauer
+ * from 00 to 23:59
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_rem;
+	int mins_rem;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+		hours_rem = hours % 10;
+		_putchar(hours / 10 + '0');
+		_putchar(':');
+		_putchar(minutes / 10 + '0');
+		_putchar(mins_rem + '0');
+		minutes++;
+		_putchar('\n')
+		}
+	hours++;
+	minutes = 0;
+	}
+}
