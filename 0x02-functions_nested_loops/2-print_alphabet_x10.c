6d6210270185edd4125c7c43675e0c4693bdb048
Selma Shikone <sellyh19@gmail.com>
2022-09-13 15:24:24 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- function to print lowecase alphabet x10
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+}
+
