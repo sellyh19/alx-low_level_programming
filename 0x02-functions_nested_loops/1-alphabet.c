e64fd5477ebbe2f077e258bc8d2352659090fe73
Selma Shikone <sellyh19@gmail.com>
2022-09-13 14:01:15 -0700

    function for alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main1.h"
+/**
+ * print_alphabet- function to print lowecase alphabet
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchard('\n');
+}
+
