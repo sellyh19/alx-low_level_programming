4a9993bbf4e2f1af7636dde797543fe2745e421a
Selma Shikone <sellyh19@gmail.com>
2022-09-13 18:01:23 -0700

    print numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98- print number from 0 to 9
 * @n: number to start counting
@@ -16,4 +17,5 @@ void print_to_98(int n)
 		while (n < 98)
 			printf("%d, ", n++);
 		printf("%d\n", n);
+	}
 }
