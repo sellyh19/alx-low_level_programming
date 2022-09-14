8835088b4efe1bb8acfeb4c9136c33d3e4f859c2
Selma Shikone <sellyh19@gmail.com>
2022-09-13 18:10:17 -0700

    9 times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,8 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		for (mult = 0; mult <= 9; mult++)
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
 		{
 		_putchar(',');
 		_putchar(' ');
